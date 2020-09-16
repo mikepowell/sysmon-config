eb435664d4ddb02a344639dc2c957cbaccec107e
Mike Powell <powell.mike@gmail.com>
2020-09-15 21:33:45 -0400

    Changing dl directory

--- install/install_sysmon.bat
+++ install/install_sysmon.bat
@@ -12,17 +12,17 @@ if "%hour:~0,1%"==" " set hour=0%hour:~1,1%
 if "%hour:~1,1%"=="" set hour=0%hour%
 if "%minute:~1,1%"=="" set minute=0%minute%
 set tasktime=%hour%:%minute%
-mkdir C:\Windows
-pushd "C:\Windows\"
+mkdir C:\ProgramData\sysmon
+pushd "C:\ProgramData\sysmon"
 echo [+] Downloading Sysmon...
-@powershell (new-object System.Net.WebClient).DownloadFile('https://live.sysinternals.com/Sysmon.exe','C:\Windows\sysmon.exe')"
+@powershell (new-object System.Net.WebClient).DownloadFile('https://live.sysinternals.com/Sysmon.exe','C:\ProgramData\sysmon\sysmon.exe')"
 echo [+] Downloading Sysmon config...
-@powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/mikepowell/sysmon-config/master/sysmonconfig-export.xml','C:\Windows\sysmonconfig-export.xml')"
-@powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/mikepowell/sysmon-config/master/install/update_sysmon.bat','C:\Windows\update_sysmon.bat')"
+@powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/mikepowell/sysmon-config/master/sysmonconfig-export.xml','C:\ProgramData\sysmon\sysmonconfig-export.xml')"
+@powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/mikepowell/sysmon-config/master/install/update_sysmon.bat','C:\ProgramData\sysmon\update_sysmon.bat')"
 sysmon.exe -accepteula -i sysmonconfig-export.xml
 sc failure Sysmon actions= restart/10000/restart/10000// reset= 120
 echo [+] Sysmon Successfully Installed!
 echo [+] Creating Auto Update Task set to Hourly..
-SchTasks /Create /RU SYSTEM /RL HIGHEST /SC HOURLY /TN Update_Sysmon_Rules /TR C:\Windows\Auto_Update_SYSMON.bat /F /ST %tasktime%
+SchTasks /Create /RU SYSTEM /RL HIGHEST /SC HOURLY /TN Update_Sysmon_Rules /TR C:\ProgramData\sysmon\update_sysmon.bat /F /ST %tasktime%
 timeout /t 10
 exit
--- install/update_sysmon.bat
+++ install/update_sysmon.bat
@@ -1,6 +1,6 @@
 @echo off
-cd C:\Windows
-@powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/mikepowell/sysmon-config/master/sysmonconfig-export.xml','C:\Windows\sysmonconfig-export.xml')"
+cd C:\ProgramData\sysmon
+@powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/mikepowell/sysmon-config/master/sysmonconfig-export.xml','C:\ProgramData\sysmon\sysmonconfig-export.xml')"
 sysmon -c sysmonconfig-export.xml
 rm sysmonconfig-export.xml
 exit
